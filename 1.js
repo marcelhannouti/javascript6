959f47589dacc43e9cfe0e31fa8d25b1f9438276
marc <marc.hannouti360@gmail.com>
2022-08-11 10:28:20 +0200

    commi

---
+++ 1.js
@@ -0,0 +1,22 @@
+
+function numero(numero1,numero2,numero3) {
+let resultado= numero1 + numero2 +numero3
+    return resultado
+}
+console.log(numero(5,8,5))
+
+function nombre(nombre,apellido1,apellido2) {
+    let resultado= nombre +" " + apellido1 +" " + apellido2
+        return resultado
+    }
+    console.log(nombre("marc","el hannouti","gordo"))
+
+function mayor (num1,num2){
+if (num1 > num2){
+    return "el numero mas grande es el numero1:" +" " + num1
+}else{
+    return "el numero mas grande es el numero2:" + " " +num2
+}
+}
+
+console.log(mayor(8,9))
